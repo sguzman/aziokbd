55af5fadad1e3c0d1d3826c695ac34ec0234806a
juanma <juanma1980@gmail.com>
2024-04-02 19:02:38 +0200

    Revert "Use cfg file for grub"
    This reverts commit 7bb75f7bdb259e6bd77cbde95399c83c61f04a99.

--- install.sh
+++ install.sh
@@ -25,23 +25,22 @@ quirk='0x0c45:0x7603:0x4'
 grubquirk="usbhid.quirks=$quirk"
 
 # Making sure the quirk does not get added multiple times
-GRUBCONF=/etc/default/grub.d/aziokbd.cfg
-if ! (cat $GRUBCONF | grep "$grubquirk"); then
-    echo '## Writing to $GRUBCONF'
-    echo "GRUB_CMDLINE_LINUX_DEFAULT=\"\$GRUB_CMDLINE_LINUX_DEFAULT $grubquirk\"" >> $GRUBCONF
-    distro=$(lsb_release -si)
-    case $distro in 
-        'Ubuntu'|'Debian'):
-            update-grub
-            ;;
-        *):
-            if [ -f "/etc/arch-release" ]; then
-                update-grub
-            elif [ -f "/etc/fedora-release" ]; then
-                grub2-mkconfig -o /boot/grub2/grub.cfg
-            fi
-	    ;;
-    esac
+if ! (cat /etc/default/grub.d/aziokbd.conf | grep "$grubquirk"); then
+    echo '## Writing to /etc/default/grub.d/aziokbd.conf ##'
+    echo $grubquirk >> /etc/default/grub.d/aziokbd.conf
+    distro = $(lsb_release -si)
+    if ($distro | grep 'Ubuntu'); then
+        update-grub
+    fi
+    if ($distro | grep 'Debian'); then
+        update-grub
+    fi
+    if [ -f "/etc/arch-release" ]; then
+        update-grub
+    fi
+    if [ -f "/etc/fedora-release" ]; then
+        grub2-mkconfig -o /boot/grub2/grub.cfg
+    fi
 else
     echo 'NOTICE - grub config file has already been updated'
 fi
